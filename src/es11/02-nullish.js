4880a474739e858e4edbaa35027e8d9e44f58985
Freddier <hch.fredy@gmail.com>
2023-04-03 22:05:59 -0500

    Added file 02-nullish.js

---
+++ src/es11/02-nullish.js
@@ -0,0 +1,8 @@
+// Operador Nullish Coalescing
+
+const anotherNumber = null;
+const validate = anotherNumber ?? 5;
+console.log(validate);
+
+// 1  --> cuando el anotherNumber tiene como valor 1
+// 5 --> cuando la variable anotherNumber tiene valor null
