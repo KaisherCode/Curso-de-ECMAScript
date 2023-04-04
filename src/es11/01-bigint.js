1e789474c0ba98250ea389388cfaa78f09209dd5
Freddier <hch.fredy@gmail.com>
2023-04-03 21:54:51 -0500

    Added file 01-bigint.js

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,12 @@
+`Big Int, enteros muy grandes`
+
+const number1 = 45n
+const number2 = BigInt(45)
+
+typeof 45n // 'bigint'
+
+const aBigNumber = 8987245920742893847n;
+const anotherBigNumber = BigInt(8987245920742893847);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
