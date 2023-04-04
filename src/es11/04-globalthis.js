553573b32a4ea774fb0e03d9dbf5823b74deb4ec
Freddier <hch.fredy@gmail.com>
2023-04-03 22:32:08 -0500

    Added file 04-globalthis.js

---
+++ src/es11/04-globalthis.js
@@ -0,0 +1,6 @@
+// Objeto global para cualquier plataforma
+
+console.log(window); // navegador
+console.log(global); // node
+console.log(self); // webworker
+console.log(globalThis); // compatible para cualquier plataforma.
