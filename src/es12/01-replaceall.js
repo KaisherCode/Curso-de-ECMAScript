999049dedf39af431283102c2623d90267f55ef5
Freddier <hch.fredy@gmail.com>
2023-04-17 17:17:27 -0500

    Added file 01-replaceall.js

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,18 @@
+const string = "JavaScript es un maravilloso lenguaje de programación";
+
+const replacedString = string.replace("JavaScript","TYpeScript");
+
+console.log(replacedString);
+
+// Otro ejemplo
+
+const mensaje = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web."
+
+mensaje.replace("JavaScript", "Python")
+// 'Python es maravilloso, con JavaScript puedo crear el futuro de la web.'
+
+mensaje.replaceAll("JavaScript", "Python")
+// 'Python es maravilloso, con Python puedo crear el futuro de la web.'
+
+mensaje.replaceAll(/a/g, "*")
+// 'J*v*Script es m*r*villoso, con J*v*Script puedo cre*r el futuro de l* web.'
