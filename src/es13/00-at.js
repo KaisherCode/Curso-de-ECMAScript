c851937be9ee0888a9dbe98eb6b3ef3a4384f0e4
Freddier <hch.fredy@gmail.com>
2023-04-17 17:43:12 -0500

    Added file 00-at.js

---
+++ src/es13/00-at.js
@@ -0,0 +1,5 @@
+// ¿Como puedes acceder al ultimo elem de este array sin saber cuantos tiene?
+const array = ["one","two","three","four","five","six"];
+console.log(array[array.length - 1])
+
+console.log(array.at(-1));
